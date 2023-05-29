a1525480f85e365aaa9a2c8a2c8aef7ce7fce857
IT-gMA <49443166+IT-gMA@users.noreply.github.com>
2023-05-29 22:50:45 +0800

    include dropout usage

--- rcnn_model_trainings/workorder_classifications/work_order_desc_semantic_analysis/model.py
+++ rcnn_model_trainings/workorder_classifications/work_order_desc_semantic_analysis/model.py
@@ -1,17 +1,19 @@
-from configs import PRETRAINED_MODEL_NAME
+from configs import PRETRAINED_MODEL_NAME, DROPOUT
 import torch
 from torch import nn, optim
 from transformers import BertModel, BertTokenizer
 
 
 class SentenceSimilarityModel(nn.Module):
-    def __init__(self):
+    def __init__(self, dropout_rate=DROPOUT):
         super(SentenceSimilarityModel, self).__init__()
         self.bert = BertModel.from_pretrained(PRETRAINED_MODEL_NAME)
+        self.dropout = nn.Dropout(dropout_rate)
         self.regression_layer = nn.Linear(self.bert.config.hidden_size, 1)
 
     def forward(self, input_ids, attention_mask, token_type_ids):
         outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
         pooled_output = outputs.pooler_output
+        pooled_output = self.dropout(pooled_output)  # Apply dropout
         similarity_score = self.regression_layer(pooled_output)
         return similarity_score
