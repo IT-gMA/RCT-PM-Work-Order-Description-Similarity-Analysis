7c93d4ffc7e3d7b0ae67928ac210ded3b756f266
IT-gMA <49443166+IT-gMA@users.noreply.github.com>
2023-06-01 20:49:43 +0800

    run iteration 2 for bert base uncased

--- rcnn_model_trainings/workorder_classifications/work_order_desc_semantic_analysis/configs.py
+++ rcnn_model_trainings/workorder_classifications/work_order_desc_semantic_analysis/configs.py
@@ -3,7 +3,7 @@ from torch import nn, optim
 from transformers import GPT2TokenizerFast, BertTokenizer
 from util_fucntions import util_functions
 
-MODEL_ITERATION = 1
+MODEL_ITERATION = 2
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps")
 print(f'Running on {DEVICE}')
@@ -16,10 +16,10 @@ TEXT2_KEY_NAME = 'pm_wo_desc'
 SAMPLE_IDX_CODE_NAME = 'mapping_code'
 
 
-PRETRAINED_MODEL_NAME = 'gpt2'
+PRETRAINED_MODEL_NAME = 'bert-base-uncased'
 
 MODEL_TOKENIZER = BertTokenizer.from_pretrained(PRETRAINED_MODEL_NAME) if 'bert' in util_functions.lower_case_and_clear_white_space(PRETRAINED_MODEL_NAME) else GPT2TokenizerFast.from_pretrained(PRETRAINED_MODEL_NAME)
-PADDING_TOKEN = '[PAD]'
+PADDING_TOKEN = None
 if type(PADDING_TOKEN) is str and not PADDING_TOKEN.isspace():
     MODEL_TOKENIZER.add_tokens([PADDING_TOKEN])
     padding_token_id = MODEL_TOKENIZER.encode(MODEL_TOKENIZER)[0]
@@ -45,10 +45,10 @@ VALIDATION_RATIO = .2
 TEST_RATIO = .15
 DATA_FILE_PATH = '../../../xlsx_resources/for_trainings/Book5.xlsx'
 
-WANDB_PROJECT_NAME = f'GPT2 Model Training Iter{MODEL_ITERATION}'
-RUNNING_LOG_LOCATION = f'saved_logs/gpt2_run_logs/running_iteration_{MODEL_ITERATION}.txt'
-SAVED_MODEL_LOCATION = f'saved_models/gpt2_models_iteration_{MODEL_ITERATION}/'
+WANDB_PROJECT_NAME = f'BERT Base Uncased Model Training Iter{MODEL_ITERATION}'
+RUNNING_LOG_LOCATION = f'saved_logs/bert_based_uncased_run_logs/running_iteration_{MODEL_ITERATION}.txt'
+SAVED_MODEL_LOCATION = f'saved_models/bert_based_uncased_models_iteration_{MODEL_ITERATION}/'
 SAVED_MODEL_FORMAT = '.pt'
 
-SAVED_UNTRAINED_SAMPLE_IDX_LOCATION = f'saved_untrained_wos/gpt2/running_iteration_{MODEL_ITERATION}.json'
-SAVED_TRAINED_SAMPLE_IDX_LOCATION = f'saved_trained_wos/gpt2/running_iteration_{MODEL_ITERATION}.json'
+SAVED_UNTRAINED_SAMPLE_IDX_LOCATION = f'saved_untrained_wos/bert_based_uncased/running_iteration_{MODEL_ITERATION}.json'
+SAVED_TRAINED_SAMPLE_IDX_LOCATION = f'saved_trained_wos/bert_based_uncased/running_iteration_{MODEL_ITERATION}.json'
